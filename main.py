f5c9aca27a95062865684c4c60990627c776b5c2
Mohammad Ayub Adar <adarayub@gmail.com>
2025-04-12 20:13:28 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+name="Mohammad Ayub Adar"
+age=27
+is_student=True
+print(type(name))
+print(type(age))
+print(type(is_student))
+a=10
+print(age+a)
+print(age-a)
+print(age**a)
+print(age/a)
+Given_value=18
+print(age>Given_value)
+print(age==Given_value)
+print(age!=Given_value)
+print(age<Given_value)
+b=20
+c=30
+print(a>b and a>c)
+print(a>b or a>c)
+print(not(print(a>b and a>c)))
+d=50
+d+=5
+print(d)
+d-=5
+print(d)
+d*=5
+print(d)
+d/=5
+print(d)
+
+x=[1,2,3]
+y=[6,7,8]
+print(x is y)
+print(x is not y)
+colors=["green","blue","red"]
+print("blue" in colors)
+print("green" not in colors)
